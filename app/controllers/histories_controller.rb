1cde0208b1f679d453b5e771aa28ec1932abae81
rajan-gith <rajan.beryls@gmail.com>
2020-12-18 22:16:44 +0530

    minor changes

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,6 +1,6 @@
 class HistoriesController < ApplicationController
   before_action :authenticate_user!
   def index
-    @histories = current_user.histories.includes(:cin).paginate(page: params[:page], per_page: 3)
+    @histories = current_user.histories.includes(:cin).paginate(page: params[:page], per_page: 10)
   end
 end
