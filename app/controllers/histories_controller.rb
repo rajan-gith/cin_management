dad9729c55729c67ac98e620d6971aa5b203b8e3
rajan-gith <rajan.beryls@gmail.com>
2020-12-18 22:12:58 +0530

    pagination added

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,6 +1,6 @@
 class HistoriesController < ApplicationController
   before_action :authenticate_user!
   def index
-    @histories = current_user.histories.includes(:cin)
+    @histories = current_user.histories.includes(:cin).paginate(page: params[:page], per_page: 3)
   end
 end
